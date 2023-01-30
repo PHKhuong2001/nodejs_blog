eb62e7207991ba2632faf2a13043269912d33fab
PHKhuong2001 <khuongph20.cdsg@saigontech.edu.vn>
2023-01-30 14:45:31 -0800

    demo

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,11 +1,11 @@
 class NewsController {
-  // [Get] /Newss
-  index(req, res) {
-    res.render("news");
-  }
-  show(req, res) {
-    res.send("New Details");
-  }
+    // [Get] /Newss
+    index(req, res) {
+        res.render('news');
+    }
+    show(req, res) {
+        res.send('New Details');
+    }
 }
 
 module.exports = new NewsController();
--- src/app/controllers/SiteControllers.js
+++ src/app/controllers/SiteControllers.js
@@ -1,12 +1,12 @@
 class SiteController {
-  // [Get] /Home
-  home(req, res) {
-    res.render("home");
-  }
-  // [Get] /Search
-  search(req, res) {
-    res.render("search");
-  }
+    // [Get] /Home
+    home(req, res) {
+        res.render('home');
+    }
+    // [Get] /Search
+    search(req, res) {
+        res.render('search');
+    }
 }
 
 module.exports = new SiteController();
